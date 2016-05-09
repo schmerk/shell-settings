875f021c8413d54d4e4f5dff0c552c42a0119267
Matt Jacobs <schmerk@users.noreply.github.com>
2016-05-09 15:57:10 -0500

    Fix git-setup-once.sh
    - Fix setting color.* settings
    - Add --prune to the up alias

--- git-setup-once.sh
+++ git-setup-once.sh
@@ -1,14 +1,14 @@
-git config --global color.diff=auto
-git config --global color.status=auto
-git config --global color.branch=auto
-git config --global color.interactive=true
-git config --global color.status=auto
-git config --global color.branch=auto
-git config --global color.interactive=auto
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.interactive true
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.interactive auto
 git config --global color.ui auto
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.st status
-git config --global alias.up 'remote update'
+git config --global alias.up 'remote update --prune'
 git config --global alias.lol 'log --oneline --graph -- decorate'
 git config --global rebase.autosquash=true
